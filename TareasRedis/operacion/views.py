debb978609dcc4b556a49191b18ef80580fb1118
Elder <eldertojins@gmail.com>
2022-06-13 13:41:46 -0600

    [ADD]Obtener datos de redis y realizar update de operacion para el resultado en la base de datos, en tarea 2

--- TareasRedis/operacion/views.py
+++ TareasRedis/operacion/views.py
@@ -4,6 +4,7 @@ from .forms import Formulario
 from .models import Operacion
 from celery import Celery
 
+
 # Create your views here.
 class Operaciones(HttpRequest):
     def inicio(request):
@@ -17,12 +18,13 @@ class Operaciones(HttpRequest):
                 app = Celery(
                     'task',
                     broker='amqp://user:bitnami@rabbitmq',
-                    #backend='rpc://user:bitnami@rabbitmq',
+                    backend='rpc://user:bitnami@rabbitmq',
                 )
                 task1 = app.send_task('addTask', (request.POST['numero1'], request.POST['numero2'], request.POST['operador']))
                 form.save()
-
-                task2 = app.send_task('updateTask')
+                result = app.AsyncResult(task1)
+                id = Operacion.objects.all().last().id
+                app.send_task('updateTask', (result.get(), id,))
 
             return render(request, "nuevo.html", {"form": form, "mensaje": "ok"})
         except Exception as e:
--- worker/worker.py
+++ worker/worker.py
@@ -1,6 +1,7 @@
 from celery import Celery
 import redis
 import uuid
+import psycopg2
 
 # Connect to our Redis instance
 redis_instance = redis.StrictRedis(host='tareasredis_cache_1',
@@ -9,7 +10,7 @@ redis_instance = redis.StrictRedis(host='tareasredis_cache_1',
 app = Celery(
     'task',
     broker='pyamqp://user:bitnami@rabbitmq',
-    #backend='rpc://user:bitnami@rabbitmq',
+    backend='rpc://user:bitnami@rabbitmq',
 )
 
 @app.task(name='addTask')
@@ -20,19 +21,45 @@ def add(numero1, numero2, operador):
         resultado = int(numero1) + int(numero2)
     elif operador == 'resta':
         resultado = int(numero1) - int(numero2)
-    elif operador == 'multiplicacion':
+    elif operador == 'multi':
         resultado = int(numero1)*int(numero2)
-    elif operador == 'division':
+    elif operador == 'div':
         resultado = int(numero1)/int(numero2)
 
     key = str(uuid.uuid4())
     redis_instance.hset(key, "numero1", numero1)
     redis_instance.hset(key, "numero2", numero2)
     redis_instance.hset(key, "resultado", resultado)
+
     return key
 
 
 @app.task(name='updateTask')
-def update():
-    print("Actualizar base de datos")
+def update(redis_id, operacion_id):
+    redis_dic = redis_instance.hgetall(str(redis_id))
+    resultado = redis_dic['resultado']
+
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="postgres",
+                                      host="db_postgres",
+                                      port="5432")
+        cursor = connection.cursor()
+        postgres_insert_query = "UPDATE operacion_operacion SET resultado =" + str(resultado) + "WHERE id=" + str(operacion_id)
+
+        cursor.execute(postgres_insert_query)
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error!!!!", error)
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+
     return None
+
+
+
